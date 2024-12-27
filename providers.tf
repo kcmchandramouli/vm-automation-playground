fee9529965bd0af5871e13f73ab839854d8468db
Chandra Mouli <chandramouli7616@gmail.com>
2024-12-27 11:14:08 +0530

    Test 9 with azure login with secrets

--- providers.tf
+++ providers.tf
@@ -10,8 +10,8 @@ terraform {
 provider "azurerm" {
   # Configuration options
   features { }
-    subscription_id = var.AZURE_SUBSCRIPTION_ID
-    client_id       = var.AZURE_CLIENT_ID
-    client_secret   = var.AZURE_CLIENT_SECRET
-    tenant_id       = var.AZURE_TENANT_ID
+    # subscription_id = var.AZURE_SUBSCRIPTION_ID
+    # client_id       = var.AZURE_CLIENT_ID
+    # client_secret   = var.AZURE_CLIENT_SECRET
+    # tenant_id       = var.AZURE_TENANT_ID
 }
