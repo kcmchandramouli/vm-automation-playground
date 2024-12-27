b3356debf32cd629a1a9c93af2dc933e93d013f2
Chandra Mouli <chandramouli7616@gmail.com>
2024-12-27 10:33:35 +0530

    test 7 Secrets as var

--- providers.tf
+++ providers.tf
@@ -10,8 +10,8 @@ terraform {
 provider "azurerm" {
   # Configuration options
   features { }
-    subscription_id = getenv("AZURE_SUBSCRIPTION_ID")
-    client_id       = getenv("AZURE_CLIENT_ID")
-    client_secret   = getenv("AZURE_CLIENT_SECRET")
-    tenant_id       = getenv("AZURE_TENANT_ID")
+    subscription_id = var.AZURE_SUBSCRIPTION_ID
+    client_id       = var.AZURE_CLIENT_ID
+    client_secret   = var.AZURE_CLIENT_SECRET
+    tenant_id       = var.AZURE_TENANT_ID
 }
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "AZURE_SUBSCRIPTION_ID" {
+  type = string
+}
+
+variable "AZURE_CLIENT_ID" {
+  type = string
+}
+
+variable "AZURE_CLIENT_SECRET" {
+  type = string
+}
+
+variable "AZURE_TENANT_ID" {
+  type = string
+}
