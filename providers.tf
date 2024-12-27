ea52722b69208572c3b7e4d846925eae5001abd0
Chandra Mouli <chandramouli7616@gmail.com>
2024-12-27 12:13:22 +0530

    Test 10

--- providers.tf
+++ providers.tf
@@ -10,4 +10,8 @@ terraform {
 provider "azurerm" {
   # Configuration options
   features { }
+  subscription_id = var.azure_subscription_id
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  tenant_id       = var.azure_tenant_id
 }
